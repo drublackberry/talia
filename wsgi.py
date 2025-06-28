5f1c740ff11b06562824cec42b422f50506a120a
drublackberry <andreu.mora@gmail.com>
2025-06-28 11:53:18 +0200

    removed .env

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
 from app import create_app
+
+# Load environment variables from config files
+# General config is loaded first, then secrets, which can override.
+load_dotenv(dotenv_path='./config.env')
+load_dotenv(dotenv_path='./secrets.env', override=True)
 
 app = create_app()
